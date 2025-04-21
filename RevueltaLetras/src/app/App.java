cde8360888e6c7f36ef40b44e10e1ae23af3d9f4
Mario Delgado <139497186+Marioderu@users.noreply.github.com>
2025-04-21 15:59:57 +0200

    Add files via upload

---
+++ RevueltaLetras/src/app/App.java
@@ -0,0 +1,21 @@
+package app;
+
+import controlador.Control;
+
+/**
+ * Clase cuyo fin es el de iniciar la aplicación llamando al Controlador.
+ * @author Mario Delgado Ruiz.
+ */
+public class App {
+	
+	/**
+	 * Método main que arranca/ejecuta la aplicación.
+	 * @param args.
+	 */
+	public static void main(String[] args) {
+		
+		Control control = new Control();
+		
+		control.start();
+	}
+}
